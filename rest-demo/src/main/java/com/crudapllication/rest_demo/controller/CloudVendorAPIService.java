d78e932814ee365bdeb6357055213ac0bb080223
gsajeendra <43973552+sajeendragavi@users.noreply.github.com>
2024-09-01 02:38:30 +0530

    Add files via upload
    Added Crud implementation

---
+++ rest-demo/src/main/java/com/crudapllication/rest_demo/controller/CloudVendorAPIService.java
@@ -0,0 +1,36 @@
+package com.crudapllication.rest_demo.controller;
+
+import com.crudapllication.rest_demo.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+    CloudVendor cloudVendor;
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId)
+    {
+        return cloudVendor;
+        //return new CloudVendor("C1", "Vendor 1","Address One", "xxxx");
+
+
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor created successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId){
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+    }
+}
